0cab29dc4b8fd129fe763f489f4eef5ce6530496
oliverblaser <oliverblaser@gmail.com>
2017-03-20 17:40:30 +0100

    First thing! Next

--- src/visualization/God.java
+++ src/visualization/God.java
@@ -23,6 +23,7 @@ import model.*;
  */
 public class God extends JPanel{
 	// This is a very good IDEA TEEEESSST
+    // tessttststts osdfjösldkfjölaksdjf
 	// Properties of the visualization
 	private final static int NUMBER_OF_AGENTS = 1000;
 	private final static int WIDTH = 1720;
